486d2da351ea79452855792870b15469e323dad3
Shreya <123@gmail.com>
2021-02-23 16:02:05 -0600

    Story Hub 1

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import readStoryScreen from './screens/readStoryScreen';
+import writeStoryScreen from './screens/writeStoryScreen';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AppContainer/>
+      </View>
+    );
+  }
 }
+
+const tabNavigator = createBottomTabNavigator({
+  readStory: {screen: readStoryScreen},
+  writeStory: {screen: writeStoryScreen}
+},
+{
+  defaultNavigationOptions: ({navigation}) => {
+    tabBarIcon: (() => {
+      const routeName = navigation.state.routeName;
+      if(routeName === "readStory") {
+        return(
+          <Image
+          source = {require("./assets/read.png")}
+          style = {{width: 40, height: 40}}/>
+        );
+      }
+      else if(routeName === "writeStory") {
+        return(
+          <Image
+          source = {require("./assets/write.png")}
+          style = {{width: 40, height: 40}}/>
+        );
+      }
+    })
+  }
+}
+);
+
+var AppContainer = createAppContainer(tabNavigator);
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/writeStoryScreen.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Header } from 'react-native-elements';
+
+export default class writeStoryScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            titleText: "",
+            authorText: "",
+            storyText: ""
+        }
+    }
+    render() {
+      return (
+        <View style={styles.container}>
+          <Header
+            backgroundColor={'#80d4ff'}
+            centerComponent={{
+              text: 'Story Hub',
+              style: { color: '#fff', fontSize: 20 },
+            }}
+          />
+           <TextInput 
+            style = {styles.inputBox} 
+            placeholder = "Story Title" 
+            value = {this.state.titleText} 
+            onChangeText = {(text) => {
+                this.setState({
+                    titleText: text
+                });
+            }}/>
+
+            <TextInput 
+            style = {styles.inputBox} 
+            placeholder = "Author" 
+            value = {this.state.authorText} 
+            onChangeText = {(text) => {
+                this.setState({
+                    authorText: text
+                });
+            }}/>
+
+            <TextInput 
+            style = {styles.inputBox} 
+            placeholder = "Write your story" 
+            value = {this.state.titleText} 
+            onChangeText = {(text) => {
+                this.setState({
+                    titleText: text
+                });
+            }}
+            multiline = {true}/>
+
+            <TouchableOpacity style = {styles.submitButton}>
+                <Text style = {styles.submitButtonText}>Submit</Text>
+            </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputBox: {
+        width: 200,
+        height: 40,
+        borderWidth: 1.5,
+        borderRightWidth: 0,
+        fontSize: 20
+    },
+    submitButton: {
+        backgroundColor: "#789efc",
+        width: 100,
+        height: 50
+    },
+    submitButtonText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: "white",
+        padding: 10
+    }
+  });
